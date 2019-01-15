39d7b69672b24262bef7f7716148ba4b2b9cd88f
hummingzz <569395086@qq.com>
2019-01-15 15:09:58 +0800

    first commit

--- preprocess.py
+++ preprocess.py
@@ -21,7 +21,7 @@ from keras.models import Sequential
 import warnings
 
 
-df_train = pd.read_csv('train.csv')
+df_train = pd.read_csv('../train.csv')
 # df_train = df_train.head(100)
 print(df_train.head())
 
@@ -60,7 +60,7 @@ def prepare_labels(y):
     # print(y.shape)
     return y, label_encoder
 
-X = prepareImages(df_train, df_train.shape[0], "train")
+X = prepareImages(df_train, df_train.shape[0], "../train")
 X /= 255
 
 y, label_encoder = prepare_labels(df_train['Id'])
